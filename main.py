4135543fc2e99fb8482ccd12a413d4cd1a854783
Erik Williams <erwilliams642003@gmail.com>
2024-03-22 22:01:00 -0800

    noodling on a recurrsion which could someday run a Monte Carlo sim of the game Memory

---
+++ main.py
@@ -0,0 +1,10 @@
+# problem: In the game of Memory, it always felt like an advantage to go second.If you were to play perfectly, what is the minimum board size so it would be advantageous to go first?
+
+def makePlay(playerNumber,piecesRemaining, totalRevealed, turnNumber)
+    # Random Probablility Match
+    # Calc Magic
+    # output Stats
+
+    if piecesRemaining > 0 {
+        makePlay(Outputstats)
+    }
